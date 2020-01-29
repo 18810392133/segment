bb5e7a05f81b15c9fcb7dee579e3efec730cc55b
2541747035@qq.com <wjl984296155@#>
2020-01-30 07:40:25 +0800

    test

--- NucleiSegmentation-master/caffe-scripts/nuclei/train_quick.sh
+++ NucleiSegmentation-master/caffe-scripts/nuclei/train_quick.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-TOOLS=./build/tools
+TOOLS=/home/hp/caffe/build/tools
 
 $TOOLS/caffe train \
-  --solver=examples/nuclei/nuclei_quick_solver.prototxt \
-  --snapshot=examples/nuclei/nuclei_quick_relative_iter_60000.solverstate 2>&1 | tee nuclei_8_run_output.txt
+  --solver=/home/hp/weijunlin/NucleiSegmentation-master_original/caffe-scripts/nuclei/nuclei_quick_solver.prototxt \
+  --snapshot=/home/hp/weijunlin/NucleiSegmentation-master_original/caffe-scripts/nuclei/weijunlin_nuclei_quick_relative_iter_60000.solverstate 2>&1 | tee weijunlin_nuclei_8_run_output.txt
 
 # reduce learning rate by factor of 10 after 8 epochs
 # $TOOLS/caffe train \
